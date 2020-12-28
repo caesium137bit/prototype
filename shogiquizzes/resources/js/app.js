251ad5bb64e9641af6b1739b67fd88accf22e78d
caesium137bit <57819815+caesium137bit@users.noreply.github.com>
2020-12-28 15:42:32 +0900

    とりあえず動く

--- shogiquizzes/resources/js/app.js
+++ shogiquizzes/resources/js/app.js
@@ -1,14 +1,11 @@
 require('./bootstrap');
 
-import BootstrapVue from 'bootstrap-vue';
-Vue.use(BootstrapVue);
-
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 import router from './router'
 
-window.Vue = Vue;
+window.Vue = require('vue');
 Vue.use(VueRouter);
 
 const app = new Vue({
