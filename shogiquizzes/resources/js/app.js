8f9b990063620cbdbaaaa099cbdb8f24a57d5ac0
caesium137bit <57819815+caesium137bit@users.noreply.github.com>
2020-12-28 16:46:28 +0900

    homeページ作成前

--- shogiquizzes/resources/js/app.js
+++ shogiquizzes/resources/js/app.js
@@ -4,11 +4,16 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 import router from './router/router'
+import store from './store/store'
+
+import BootstrapVue from 'bootstrap-vue';
+Vue.use(BootstrapVue);
 
 window.Vue = require('vue');
 Vue.use(VueRouter);
 
 const app = new Vue({
     el: '#app',
-    router
+    router,
+    store
 });;
