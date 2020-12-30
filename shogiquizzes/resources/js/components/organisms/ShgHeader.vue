1641d619fefb63c3d5190b391b6d1ce6d8eebf93
caesium137bit <57819815+caesium137bit@users.noreply.github.com>
2020-12-30 16:03:04 +0900

    logo(色々まだ)

---
+++ shogiquizzes/resources/js/components/organisms/ShgHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <ShgLogo/>
+    <ShgNavigationVar/> 
+  </div>
+</template>
+
+<script>
+import ShgLogo from 'components/atoms/ShgLogo.vue'
+import ShgNavigationVar from 'components/molecules/ShgNavigationVar.vue'
+
+export default {}
+</script>
