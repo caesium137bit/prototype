6be531270dd680412c15c64cbd15ca0a939d988e
caesium137bit <57819815+caesium137bit@users.noreply.github.com>
2020-12-31 17:46:05 +0900

    問題集途中まで

--- shogiquizzes/resources/js/components/templates/ShgQuizzes.vue
+++ shogiquizzes/resources/js/components/templates/ShgQuizzes.vue
@@ -1,6 +1,18 @@
 <template>
-  <div>
-    <h1 class="text-danger">Comming Soon!</h1>
+  <div class="container py-4">
+    <!-- カードの枠：card -->
+    <div class="card mx-auto" style="max-width: 25rem;">
+      <!-- カード上部にレイアウトされる画像：card-img-top -->
+      <img class="card-img-top" :src="'/images/kyokumen/1-1.png'" alt="1-1">
+      <!-- カード本文：card-body -->
+      <div class="card-body">
+        <!-- カード見出し：card-title -->
+        <h4 class="card-title">問題集1問題1</h4>
+        <!-- カードの内容文：card-text -->
+        <p class="card-text">問題選択肢</p>
+        <a href="#" class="btn btn-primary">次へ</a>
+      </div>
+    </div>
   </div>
 </template>
 
