adb02290a374a66aa936a76092e3230dee012a33
caesium137bit <57819815+caesium137bit@users.noreply.github.com>
2020-12-31 14:52:54 +0900

    Home完成

--- shogiquizzes/resources/js/router/router.js
+++ shogiquizzes/resources/js/router/router.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-import ShgLogo from '../components/atoms/ShgLogo.vue'
-import ShgNavigationVar from '../components/molecules/ShgNavigationVar.vue'
 import ShgHome from '../components/templates/ShgHome.vue'
 
 Vue.use(VueRouter)
