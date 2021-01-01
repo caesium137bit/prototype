876ef6a175c9ece631ac5e4b9f46917aeedf53c7
caesium137bit <57819815+caesium137bit@users.noreply.github.com>
2021-01-01 14:55:38 +0900

    カードヘッダー追加

--- shogiquizzes/resources/js/components/templates/ShgQuizzes.vue
+++ shogiquizzes/resources/js/components/templates/ShgQuizzes.vue
@@ -1,10 +1,8 @@
 <template>
 <div class="container py-4">
   <div class="card mx-auto" style="max-width: 25rem;">
-    <div class="card-body">
-      <h4 class="card-title">問題集1問題1</h4>
-    </div>
-    <img :src="'/images/kyokumen/1-1.png'" alt="1-1">
+    <h4 class="card-header">問題集1問題1</h4>
+    <img class="card-img-top" :src="'/images/kyokumen/1-1.png'" alt="1-1">
     <div class="card-body">
       <p class="card-text">問題選択肢</p>
       <a href="#" class="btn btn-primary">次へ</a>
