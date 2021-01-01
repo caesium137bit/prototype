b7ae844fcd8432b4da24180a1bde71a3252a116a
caesium137bit <57819815+caesium137bit@users.noreply.github.com>
2021-01-01 17:11:11 +0900

    問題見た目だけ完成

--- shogiquizzes/resources/js/components/templates/ShgQuizzes.vue
+++ shogiquizzes/resources/js/components/templates/ShgQuizzes.vue
@@ -1,14 +1,18 @@
 <template>
-<div class="container py-4">
-  <div class="card mx-auto" style="max-width: 25rem;">
-    <h4 class="card-header">問題集1問題1</h4>
-    <img class="card-img-top" :src="'/images/kyokumen/1-1.png'" alt="1-1">
-    <div class="card-body">
-      <p class="card-text">問題選択肢</p>
-      <a href="#" class="btn btn-primary">次へ</a>
+  <div class="container py-4">
+    <div class="card mx-auto" style="max-width: 25rem;">
+      <h4 class="card-header">問題集1問題1</h4>
+      <img class="card-img-top" :src="'/images/kyokumen/1-1.png'" alt="1-1">
+      <div class="card-body">
+        <div class="list-group">     
+          <button type="button" class="list-group-item list-group-item-action list-group-item-primary text-center text-weight-bold active" value="6六歩">6六歩</button>
+          <button type="button" class="list-group-item list-group-item-action list-group-item-primary text-center text-weight-bold" value="7九玉">7九玉</button>
+          <button type="button" class="list-group-item list-group-item-action list-group-item-primary text-center text-weight-bold" value="3五歩">3五歩</button>
+          <button type="button" class="list-group-item list-group-item-action list-group-item-primary text-center text-weight-bold" value="4五桂馬">4五桂馬</button>  
+        </div>
+      </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
