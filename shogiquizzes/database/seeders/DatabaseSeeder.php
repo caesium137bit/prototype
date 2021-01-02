6e5a7c2e6ce089360d90c5377934c9b448b27369
caesium137bit <57819815+caesium137bit@users.noreply.github.com>
2021-01-02 10:32:12 +0900

    seederファイル登録実行後

--- shogiquizzes/database/seeders/DatabaseSeeder.php
+++ shogiquizzes/database/seeders/DatabaseSeeder.php
@@ -13,6 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(SectionsTableSeeder::class);
+        $this->call(QuizzesTableSeeder::class);
+        $this->call(ChoicesTableSeeder::class);
         // \App\Models\User::factory(10)->create();
     }
 }
