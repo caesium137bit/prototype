34cdb44a5e719f83ad52288ca6ea83b403d1750c
caesium137bit <57819815+caesium137bit@users.noreply.github.com>
2021-01-04 21:26:31 +0900

    配列からのデータの取得完了

--- shogiquizzes/resources/js/components/templates/ShgQuizzes.vue
+++ shogiquizzes/resources/js/components/templates/ShgQuizzes.vue
@@ -1,7 +1,12 @@
-<template>
-  <div class="container py-4">
-    <div class="card mx-auto" style="max-width: 25rem;">
-      <h4 class="card-header">{{quizzes[0].title}}</h4>
+<template> 
+  <div class="card mx-auto" style="max-width: 25rem;">
+      <h4 class="card-header">Section{{section[0].id}} {{section[0].title}} Part{{section[0].quizzes[0].order}}</h4>
+      <img class="card-img-top" v-bind:src="section[0].quizzes[0].image_src" alt="局面図">
+      <div class="card-body">
+        <div class="list-group">     
+          <button type="button" class="list-group-item list-group-item-action list-group-item-primary text-center text-weight-bold active" v-bind:value="section[0].quizzes[0].choices[0].choice">{{section[0].quizzes[0].choices[0].choice}}</button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -10,19 +15,26 @@
   export default {
       data: function () {
           return {
-              quizzes: []
+              section: []
           }
       },
       methods: {
-          getQuizzes() {
+          getSection() {
               axios.get('/api/quizzes')
                   .then((res) => {
-                      this.quizzes = res.data;
+                      this.section = res.data;
                   });
           },
       },
       mounted() {
-          this.getQuizzes();
+          this.getSection();
       }
   }
 </script>
+
+<div class="container py-4">
+    <div class="card mx-auto" style="max-width: 25rem;">
+      <h4 class="card-header">{{quizzes[0].title}}</h4>
+    </div>
+  </div>
+  
