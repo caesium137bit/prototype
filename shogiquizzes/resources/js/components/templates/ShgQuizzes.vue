4212561f8ab293152f5a510fb7a970705cb45035
caesium137bit <57819815+caesium137bit@users.noreply.github.com>
2021-01-11 17:49:24 +0900

    onClickChoice途中まで実装

--- shogiquizzes/resources/js/components/templates/ShgQuizzes.vue
+++ shogiquizzes/resources/js/components/templates/ShgQuizzes.vue
@@ -1,10 +1,10 @@
 <template> 
   <div class="card mx-auto" style="max-width: 25rem;">
-      <h4 class="card-header">Section{{section[0].id}} {{section[0].title}} Part{{section[0].quizzes[0].order}}</h4>
-      <img class="card-img-top" v-bind:src="section[0].quizzes[0].image_src" alt="局面図">
+      <h4 class="card-header">クイズの長さ {{quizLen}} Section{{section[sectionNo].id}} {{section[sectionNo].title}} Part{{section[sectionNo].quizzes[quizNo].order}}</h4>
+      <img class="card-img-top" v-bind:src="section[sectionNo].quizzes[quizNo].image_src" alt="局面図">
       <div class="card-body">
         <div class="list-group">     
-          <button type="button" class="list-group-item list-group-item-action list-group-item-primary text-center text-weight-bold active" v-bind:value="section[0].quizzes[0].choices[0].choice">{{section[0].quizzes[0].choices[0].choice}}</button>
+          <button type="button" class="list-group-item list-group-item-action list-group-item-primary text-center text-weight-bold active" v-bind:value="section[sectionNo].quizzes[quizNo].choices[choiceNo].choice" v-on:click="onClickChoice">{{section[sectionNo].quizzes[quizNo].choices[choiceNo].choice}}</button>
         </div>
       </div>
     </div>
@@ -15,26 +15,32 @@
   export default {
       data: function () {
           return {
-              section: []
+              section: [],
+              sectionID: 1,             
+              sectionNo: 0,
+              quizNo: 0,
+              choiceNo: 0,
+              correctCount: 0
+          }
+      },     
+      computed: {
+          quizLen: function () {
+              return Object.keys(this.section).length
           }
       },
       methods: {
           getSection() {
-              axios.get('/api/quizzes?id=1')
+              axios.get('/api/quizzes?id=' + this.sectionID)
                   .then((res) => {
                       this.section = res.data;
                   });
           },
+          onClickChoice: function (event) {
+              this.quizNo += 1
+          }
       },
       mounted() {
           this.getSection();
       }
   }
 </script>
-
-<div class="container py-4">
-    <div class="card mx-auto" style="max-width: 25rem;">
-      <h4 class="card-header">{{quizzes[0].title}}</h4>
-    </div>
-  </div>
-  
