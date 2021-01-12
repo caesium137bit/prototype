43e6aeba16914517a4813c55ac985edae3265233
caesium137bit <57819815+caesium137bit@users.noreply.github.com>
2021-01-12 15:12:31 +0900

    問題フッター完成

--- shogiquizzes/resources/js/components/templates/ShgQuizzes.vue
+++ shogiquizzes/resources/js/components/templates/ShgQuizzes.vue
@@ -1,12 +1,17 @@
 <template> 
   <div class="container py-4">
-    <div class="card mx-auto" style="max-width: 25rem;">
-      <h4 class="card-header">クイズの長さ {{quizLen}} 正解数{{correctCount}} Section{{section[sectionNo].id}} {{section[sectionNo].title}} Part{{section[sectionNo].quizzes[quizNo].order}}</h4>
+    <div class="card border-primary mx-auto" style="max-width: 25rem;">
+      <div class="card-header border-primary">
+        <h4>クイズの長さ {{quizLen}} 正解数{{correctCount}} Section{{section[sectionNo].id}} {{section[sectionNo].title}} Part{{section[sectionNo].quizzes[quizNo].order}}</h4>
+      </div>
       <img class="card-img-top" v-bind:src="section[sectionNo].quizzes[quizNo].image_src" alt="局面図">
       <div class="card-body">
         <div class="list-group" v-for="choice in section[sectionNo].quizzes[quizNo].choices"  v-bind:key="choice.id">     
           <button type="button" class="list-group-item list-group-item-action list-group-item-primary text-center text-weight-bold active" v-bind:value="choice.choice" v-on:click="onClickChoice($event)">{{choice.choice}}</button>
         </div>
+      </div>
+      <div class="card-footer border-primary">
+        <h4>{{quizNo}}問中{{correctCount}}問正解!</h4>
       </div>
     </div>
   </div>
