9b670bda4d45bb6a1e5207b423c02db55b9927dc
caesium137bit <57819815+caesium137bit@users.noreply.github.com>
2021-01-24 09:37:40 +0900

    プロジェクト名修正後

--- shoquizzes/resources/js/components/atoms/ShgLogo.vue
+++
@@ -1,9 +0,0 @@
-<template> 
-  <div class="container text-center py-4">
-    <h1><router-link to="/"><img :src="'/images/logo.png'" width="400" height="100" alt="AI将棋道場"></router-link></h1>
-  </div>  
-</template>
-
-<script>
-  export default {}
-</script>
